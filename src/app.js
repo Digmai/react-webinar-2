eb43e2050a0003ba24bb64c38419b21388a5b348
Digmai <60818120+Digmai@users.noreply.github.com>
2022-08-06 02:45:03 +0300

    Revert "Lecture2"

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
-import React, {useCallback} from 'react';
-import Controls from "./components/controls";
-import List from "./components/list";
-import Layout from "./components/layout";
-import {counter} from "./utils";
+import React from 'react';
+import {counter} from './utils.js';
+import './style.css';
 
 /**
  * Приложение
@@ -10,28 +8,38 @@ import {counter} from "./utils";
  * @return {React.ReactElement} Виртуальные элементы React
  */
 function App({store}) {
-
-  const callbacks = {
-    onAdd: useCallback(() => {
-      const code = counter();
-      store.createItem({code, title: `Новая запись ${code}`});
-    }, []),
-    onSelectItems: useCallback((code) => {
-      store.selectItem(code);
-    }, []),
-    onDeleteItems: useCallback((code) => {
-      store.deleteItem(code);
-    }, []),
-  }
+  // Выбор состояния из store
+  const {items} = store.getState();
 
   return (
-    <Layout head={<h1>Приложение на чистом JS</h1>}>
-      <Controls onAdd={callbacks.onAdd}/>
-      <List items={store.getState().items}
-            onItemSelect={callbacks.onSelectItems}
-            onItemDelete={callbacks.onDeleteItems}
-      />
-    </Layout>
+    <div className='App'>
+      <div className='App__head'>
+        <h1>Приложение на чистом JS</h1>
+      </div>
+      <div className='Controls'>
+        <button onClick={() => {
+          const code = counter();
+          store.createItem({code, title: `Новая запись ${code}`})
+        }}> Добавить </button>
+      </div>
+      <div className='App__center'>
+        <div className='List'>{items.map(item =>
+          <div key={item.code} className='List__item'>
+            <div className={'Item' + (item.selected ? ' Item_selected' : '')}
+                 onClick={() => store.selectItem(item.code)}>
+              <div className='Item__number'>{item.code}</div>
+              <div className='Item__title'>{item.title}</div>
+              <div className='Item__actions'>
+                <button onClick={() => store.deleteItem(item.code)}>
+                  Удалить
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ class Store {
     // Состояние приложения (данные)
     this.state = initState;
     // Слушатели изменений state
-    this.listeners = [];
+    this.listners = [];
   }
 
   /**
@@ -22,8 +22,8 @@ class Store {
   setState(newState) {
     this.state = newState;
     // Оповещаем всех подписчиков об изменении стейта
-    for (const listener of this.listeners) {
-      listener();
+    for (const lister of this.listners) {
+      lister();
     }
   }
 
@@ -33,20 +33,20 @@ class Store {
    * @return {Function} Функция для отписки
    */
   subscribe(callback) {
-    this.listeners.push(callback);
+    this.listners.push(callback);
     // Возвращаем функцию для удаления слушателя
     return () => {
-      this.listeners = this.listeners.filter(item => item !== callback);
+      this.listners = this.listners.filter(item => item !== callback);
     }
   }
 
   /**
    * Создание записи
    */
-  createItem({code, title = 'Новый товар', price = 999, selected = false}) {
+  createItem({code, title = 'Новая запись', selected = false}) {
     this.setState({
       ...this.state,
-      items: this.state.items.concat({code, title, price, selected})
+      items: this.state.items.concat({code, title, selected})
     });
   }
 
@@ -70,13 +70,9 @@ class Store {
       ...this.state,
       items: this.state.items.map(item => {
         if (item.code === code){
-          return {
-            ...item,
-            selected: !item.selected,
-            count: item.selected ? item.count : item.count + 1 || 1
-          }
+          item.selected = !item.selected;
         }
-        return item.selected ? {...item, selected: false} : item;
+        return item;
       })
     });
   }
